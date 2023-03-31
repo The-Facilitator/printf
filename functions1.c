16f786d1f27285c30d7e16cbc2531dcd77f92eab
Akor Hilary Okwute <cyberakor@gmail.com>
2023-03-31 16:17:30 -0700

    functions1.c

--- functions1.c
+++ functions1.c
@@ -15,6 +15,7 @@
  *
  * Return: Number of chars printed.
  */
+
 int print_unsigned(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
@@ -52,6 +53,7 @@ int print_unsigned(va_list types, char buffer[],
  *
  * Return: Number of chars printed
  */
+
 int print_octal(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
@@ -96,6 +98,7 @@ int print_octal(va_list types, char buffer[],
  *
  * Return: Number of chars printed
  */
+
 int print_hexadecimal(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
@@ -115,6 +118,7 @@ int print_hexadecimal(va_list types, char buffer[],
  * @size: Size specifier
  * Return: Number of chars printed
  */
+
 int print_hexa_upper(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
@@ -137,6 +141,7 @@ int print_hexa_upper(va_list types, char buffer[],
  * @size: Size specification
  * Return: Number of chars printed
  */
+
 int print_hexa(va_list types, char map_to[], char buffer[],
 	int flags, char flag_ch, int width, int precision, int size)
 {
