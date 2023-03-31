8eb3cb272229343d9c4308465cc266b823be95bf
Akor Hilary Okwute <cyberakor@gmail.com>
2023-03-31 15:39:15 -0700

    Update

--- get_precision.c
+++ get_precision.c
@@ -3,6 +3,7 @@
 /**
  * get_precision - Calculates the precision for printing and returns the
  * result to the stdout
+ *
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  * @list: list of arguments.
